0ca93fff1c7f67557a5ef119f02cbb5cca1385a5
Daniel Mahefasoa Brady <dmbrady01@gmail.com>
2018-03-01 21:58:47 -0500

    Finished tests for EvtDict

--- tests/test_event_processing.py
+++ tests/test_event_processing.py
@@ -11,15 +11,47 @@ __lastmodified__ = "01 Mar 2018"
 
 # Import unittest modules and event_processing
 import unittest
-from photon_analysis import event_processing
+from photon_analysis.event_processing import EvtDict
 
-class TestEventProcessing(unittest.TestCase):
+class TestEvtDict(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.list1 = ['event1', 'event2']
+        self.dict1 = {'1': 'event1', '2': 'event2'}
 
     def tearDown(self):
-        pass
+        del self.list1
+        del self.dict1
+
+    def test_returns_a_dict(self):
+        "Makes sure EvtDict returns a dictionary"
+        self.assertIsInstance(EvtDict(), dict)
+
+    def test_dict_length_same_as_given_list(self):
+        "Makes sure the dictionary length is equal to given list length"
+        self.assertEqual(len(EvtDict(self.list1)), len(self.list1))
+
+    def test_dict_key_begins_at_one(self):
+        "Makes sure the first key is 1"
+        results = EvtDict(self.list1)
+        keys = [int(key) for key in results.keys()]
+        self.assertEqual(min(keys), 1)
+
+    def test_keys_are_strings(self):
+        "Makes sure keys are strings (not integers or floats)"
+        keys = EvtDict().keys()
+        str_check = [isinstance(key, str) for key in keys]
+        self.assertTrue(all(str_check))
+
+    def test_returns_properly_ordered_dict(self):
+        "Makes sure that the list is mapped to the dictionary in the right order"
+        self.assertEqual(EvtDict(self.list1), self.dict1)
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
