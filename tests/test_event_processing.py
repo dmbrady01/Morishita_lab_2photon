bc6a264f1eed0c9c2d53093a246a2c4e1b9ca303
Daniel Mahefasoa Brady <dmbrady01@gmail.com>
2018-03-01 21:18:05 -0500

    Added test structure

---
+++ tests/test_event_processing.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_event_processing.py: Python script that contains tests for event_processing.py 
+"""
+
+__author__ = "DM Brady"
+__datewritten__ = "01 Mar 2018"
+__lastmodified__ = "01 Mar 2018"
+
+# Import unittest modules and event_processing
+import unittest
+from photon_analysis import event_processing
+
+class TestEventProcessing(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
