c23cc0958fe6da0e5ccd204d9f6fbc1bf3ba8714
Daniel Mahefasoa Brady <dmbrady01@gmail.com>
2018-03-05 10:15:38 -0500

    Added TruncateSignal function

--- tests/test_signal_processing.py
+++ tests/test_signal_processing.py
@@ -7,28 +7,44 @@ test_signal_processing.py: Python script that contains tests for signal_processi
 
 __author__ = "DM Brady"
 __datewritten__ = "01 Mar 2018"
-__lastmodified__ = "01 Mar 2018"
+__lastmodified__ = "05 Mar 2018"
 
 # Import unittest modules and event_processing
 import unittest
-from imaging_analysis.signal_processing import NormalizeSignal
+from neo.core import AnalogSignal
+import numpy as np
+import quantities as pq
+from imaging_analysis.signal_processing import TruncateSignal
 
-class TestSubtractNoise(unittest.TestCase):
-    pass
+class TestTruncateSignal(unittest.TestCase):
+    "Tests for the TruncateSignal function."
 
-
-class TestNormalizeSignalData(unittest.TestCase):
-    "Code tests for NormalizeSignalData function"
-    
     def setUp(self):
-        pass
+        self.signal = AnalogSignal(np.random.randn(1000,1), units='V', sampling_rate=1*pq.Hz)
+        self.not_analog = np.random.randn(1000, 1)
+        self.signal_start = 10
+        self.signal_end = 10
 
     def tearDown(self):
-        pass
+        del self.signal
+        del self.not_analog
+        del self.signal_start
+        del self.signal_end
 
-    def test_returns_a_dict(self):
-        pass
+    def test_makes_sure_analog_signal_is_passed(self):
+        "Test to make sure analog signal object is passed"
+        self.assertRaises(TypeError, TruncateSignal, self.not_analog)
 
+    def test_signal_start_works(self):
+        "Test to make sure start is truncated"
+        trunc_sig = TruncateSignal(self.signal, start=self.signal_start)
+        self.assertEqual(trunc_sig.t_start, self.signal_start * pq.s)
+
+    def test_signal_end_works(self):
+        "Test to make sure start is truncated"
+        trunc_sig = TruncateSignal(self.signal, end=self.signal_end)
+        true_stop = self.signal.t_stop - self.signal_end * pq.s
+        self.assertEqual(trunc_sig.t_stop, true_stop)
 
 
 
