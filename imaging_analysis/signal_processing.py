6b3714e022353ee05944dfec52a9c103639155d4
Daniel Mahefasoa Brady <dmbrady01@gmail.com>
2018-03-07 12:52:32 -0500

    Added ButterFilterDesign function

--- imaging_analysis/signal_processing.py
+++ imaging_analysis/signal_processing.py
@@ -9,7 +9,7 @@ processing.
 
 __author__ = "DM Brady"
 __datewritten__ = "01 Mar 2018"
-__lastmodified__ = "06 Mar 2018"
+__lastmodified__ = "07 Mar 2018"
 
 
 import scipy.signal as ssp
@@ -34,7 +34,6 @@ def TruncateSignal(signal, start=0, end=0):
 
     return truncated_signal
 
-
 def TruncateSignals(signal_list, start=0, end=0):
     """Given a list of AnalogSignal objects, will iterate through each one and remove
     values before 'start' and after 'end'. Start and end must be in seconds."""
@@ -45,6 +44,51 @@ def TruncateSignals(signal_list, start=0, end=0):
     truncated_list = [TruncateSignal(sig, start=start, end=end) for sig in signal_list]
     
     return truncated_list
+
+def ButterFilterDesign(lowcut=None, highcut=None, fs=381.469726562, order=5, 
+                    btype='lowpass'):
+    """Constructs the numerator and denominator for the low/high/bandpass/bandstop
+    filter. low and high are the cutoff frequencies, fs is the sampling frequency, 
+    order is the desired filter order, and btype designates what type of filter will
+    ultimately be constructed (lowpass, highpass, bandpass, bandstop)."""
+    # calculate nyquist frequency (half the sampling frequency)
+    nyquist = fs * 0.5
+    # try constructing normalized lowcut
+    try:
+        norm_low = lowcut / nyquist
+    except TypeError:
+        pass
+    # try constructing normalized highcut
+    try:
+        norm_high = highcut / nyquist
+        # Makes sure highcut is lower than nyquist frequency
+        if norm_high > 1:
+            raise ValueError('%s is larger than the nyquist frequency. Choose a smaller highcut (at most half the sampling rate).' % highcut)
+    except TypeError:
+        pass
+    # Construct param to be bassed into butter
+    if btype == 'lowpass':
+        params = norm_high
+    elif btype == 'highpass':
+        params = norm_low
+    elif (btype == 'bandpass') or (btype == 'bandstop'):
+        params = [norm_low, norm_high]
+    else:
+        raise ValueError("%s must be 'lowpass', 'highpass', 'bandpass', or 'bandstop'" % btype)
+    # Return butter
+    return ssp.butter(order, params, btype=btype, analog=False)
+
+
+def FilterSignal(signal, type='detrend_lowpass', **kwargs):
+    """Will filter an AnalogSignal object. There are two types of filters:
+    'detrend_lowpass': passes signal through a lowpass filter (removes high freq)
+                       and subtracts Savitzky-Golay filter of original signal (detrend)
+    'bandpass': passes signal through a bandpass filter (removes high/low freq)
+    Optional arguments are for specifying filter parameters."""
+    # Default kwarg params:
+    options = {'window_length': 3001, 'poly_order': 1, 'axis': 0}
+    if not isinstance(signal, neo.core.AnalogSignal):
+        raise TypeError('%s must be an AnalogSignal object' % signal)
 
 
 # def NormalizeSignal(signal=None, reference=None, framelen=3001, order=1, return_filt=False):
--- tests/test_signal_processing.py
+++ tests/test_signal_processing.py
@@ -7,21 +7,25 @@ test_signal_processing.py: Python script that contains tests for signal_processi
 
 __author__ = "DM Brady"
 __datewritten__ = "01 Mar 2018"
-__lastmodified__ = "06 Mar 2018"
+__lastmodified__ = "07 Mar 2018"
 
 # Import unittest modules and event_processing
 import unittest
 from neo.core import AnalogSignal
 import numpy as np
 import quantities as pq
+import scipy.signal as ssp
 from imaging_analysis.signal_processing import TruncateSignal
 from imaging_analysis.signal_processing import TruncateSignals
+from imaging_analysis.signal_processing import ButterFilterDesign
+from imaging_analysis.signal_processing import FilterSignal
 
 class TestTruncateSignal(unittest.TestCase):
     "Tests for the TruncateSignal function."
 
     def setUp(self):
-        self.signal = AnalogSignal(np.random.randn(1000,1), units='V', sampling_rate=1*pq.Hz)
+        self.signal = AnalogSignal(np.random.randn(1000,1), units='V',
+                                    sampling_rate=1*pq.Hz)
         self.not_analog = np.random.randn(1000, 1)
         self.signal_start = 10
         self.signal_end = 10
@@ -83,6 +87,84 @@ class TestTruncateSignals(unittest.TestCase):
         true_stops = [x.t_stop - self.signal_end * pq.s for x in self.signals]
         paired = zip(t_stops, true_stops)
         self.assertTrue(all(x == y for x, y in paired))
+
+
+
+class TestButterFilterDesign(unittest.TestCase):
+    "Code tests for ButterFilterDesign."
+
+    def setUp(self):
+        self.order = 10
+        self.fs = 30
+        self.high = 10
+        self.low = .5
+        self.wrong_btype = 'hamburger'
+
+    def tearDown(self):
+        del self.order
+        del self.fs
+        del self.low
+        del self.high
+        del self.wrong_btype
+
+    def test_make_sure_correct_btype_is_passed(self):
+        "Makes sure btype must be correct"
+        self.assertRaises(ValueError, ButterFilterDesign, btype=self.wrong_btype)
+
+    def test_make_sure_highcut_is_below_nyquist(self):
+        "Makes sure highcut is below nyquist frequency"
+        self.assertRaises(ValueError, ButterFilterDesign, highcut=self.fs, fs=self.fs)
+
+    def test_lowpass_filter_design_works(self):
+        "Makes sure lowpass filter design works"
+        output = ButterFilterDesign(highcut=self.high, fs=self.fs, order=self.order, 
+                                btype='lowpass')
+        check = ssp.butter(self.order, self.high/(self.fs*.5), btype='lowpass')
+        self.assertTrue(np.array_equal(output, check))
+
+    def test_highpass_filter_design_works(self):
+        "Makes sure highpass filter design works"
+        output = ButterFilterDesign(lowcut=self.low, fs=self.fs, order=self.order, 
+                                btype='highpass')
+        check = ssp.butter(self.order, self.low/(self.fs*.5), btype='highpass')
+        self.assertTrue(np.array_equal(output, check))
+
+    def test_bandpass_filter_design_works(self):
+        "Makes sure bandpass filter design works"
+        output = ButterFilterDesign(lowcut=self.low, highcut=self.high, fs=self.fs, 
+                                order=self.order, btype='bandpass')
+        check = ssp.butter(self.order, 
+                            [self.low/(self.fs*.5), self.high/(self.fs*.5)], 
+                            btype='bandpass')
+        self.assertTrue(np.array_equal(output, check))
+
+    def test_bandstop_filter_design_works(self):
+        "Makes sure bandstop filter design works"
+        output = ButterFilterDesign(lowcut=self.low, highcut=self.high, fs=self.fs, 
+                                order=self.order, btype='bandstop')
+        check = ssp.butter(self.order, 
+                            [self.low/(self.fs*.5), self.high/(self.fs*.5)], 
+                            btype='bandstop')
+        self.assertTrue(np.array_equal(output, check))
+
+
+
+class TestFilterSignal(unittest.TestCase):
+    "Code tests for FilterSignal function."
+
+    def setUp(self):
+        self.signal = AnalogSignal(np.random.randn(1000,1), units='V', 
+                                    sampling_rate=1*pq.Hz)
+        self.signal2 = AnalogSignal(np.random.randn(900,1), units='V', 
+                                    sampling_rate=1*pq.Hz)
+
+    def tearDown(self):
+        del self.signal
+        del self.signal2
+
+    def test_makes_sure_signal_is_passed(self):
+        "Makes sure signal is passed to function"
+        self.assertRaises(TypeError, FilterSignal, 100)
 
 
 
