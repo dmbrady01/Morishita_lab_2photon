0eeaa335301d2677dc3278c5e8c4a9e09985ad71
Daniel Mahefasoa Brady <dmbrady01@gmail.com>
2018-03-07 22:07:27 -0500

    Started plotting functions

---
+++ imaging_analysis/plotting.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+plotting.py: Python script that contains functions for plotting data.
+"""
+
+
+__author__ = "DM Brady"
+__datewritten__ = "07 Mar 2018"
+__lastmodified__ = "07 Mar 2018"
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def PlotSignalProcessing(signal_list=[], time=None, ylabels=[], titles=[]):
+    """Given a list (or even list of lists if you want to plot several together), 
+    creates a single figure for looking at Signal data."""
+    if not isinstance(signal_list, list):
+        raise TypeError('%s must be a list' % signal_list)
+    # Sets background style
+    sns.set_style('whitegrid')
+    # Calculates number of plots
+    num_plots = len(signal_list) 
+    # Prepare subplot indexing
+    index = num_plots * 100 + 11
+    # Starts figure
+    fig = plt.figure()
+    for x in range(num_plots):
+        # sets up first graph
+        if x == 0:
+            original_axis = fig.add_subplot(index + x)
+            plt.plot(time)
+
+        axis = index + x
---
+++ tests/test_plotting.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_plotting.py: Python script that contains tests for plotting.py 
+"""
+
+__author__ = "DM Brady"
+__datewritten__ = "07 Mar 2018"
+__lastmodified__ = "07 Mar 2018"
+
+# Import unittest modules
+import unittest
+
+class TestPlotSignals(unittest.TestCase):
+    "Tests for the PlotSignals function."
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_makes_sure_analog_signal_is_passed(self):
+        "Test to make sure analog signal object is passed"
+        pass
