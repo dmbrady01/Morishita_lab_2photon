4f7bdb37542f8167b7fe64e924a193f8d22fca86
Daniel Mahefasoa Brady <dmbrady01@gmail.com>
2019-06-24 19:56:36 -0400

    Finished regex section except for timing portion

--- get_behavioral_events.py
+++ get_behavioral_events.py
@@ -8,7 +8,7 @@ class GetBehavioralEvents(object):
 
     def __init__(self, datapath=None, savefolder=None, time_offset=0, 
         time_column='Trial time', minimum_bout_time=1, dtype='ethovision',
-        name_match=r'\d{5,}-\d*'):
+        name_match=r'\d{5,}-\d*', zone_match=r'zone'):
         # Timing info
         self.time_offset = time_offset
         self.time_column = time_column
@@ -19,7 +19,6 @@ class GetBehavioralEvents(object):
         self.dtype = dtype
         # Animal info
         self.name_match = name_match
-        self.regexp = None
 
     def set_datapath(self):
         if self.datapath is None:
@@ -31,15 +30,20 @@ class GetBehavioralEvents(object):
         if self.savefolder is None:
             self.savefolder = os.sep.join(self.datapath.split(os.sep)[:-1]) + os.sep
 
-    def compile_name_match(self):
-        self.regexp = re.compile(self.name_match)
-
     def save_files(self, dataset):
         for data in dataset:
             animal_name = data[0]
             df = data[1]
 
-            df.to_csv(self.savefolder + self.dtype + '_' + animal_name + '.csv', index=False)
+            df.to_csv(self.savefolder + self.dtype + '_' + animal_name + '.csv', 
+                index=False)
+
+    def prune_minimum_bouts(self, df):
+        return df.loc[df['Bout end'] - df['Bout start'] >= self.minimum_bout_time]
+
+    @staticmethod
+    def clean_and_strip_string(string, sep=' '):
+        sep.join(string.split())
 
     def process_ethovision(self):
         # Reads the dataframe to figure out how many rows to skip
@@ -85,7 +89,7 @@ class GetBehavioralEvents(object):
         results_df.reset_index(drop=True, inplace=True)
 
         # Make sure bouts are above minimum threshold
-        results_df = results_df.loc[results_df['Bout end'] - results_df['Bout start'] >= self.minimum_bout_time]
+        results_df = self.prune_minimum_bouts(results_df)
 
         return [(animal_name, results_df)]
 
@@ -93,10 +97,35 @@ class GetBehavioralEvents(object):
         with open(self.datapath, 'r') as fp:
             contents = fp.readlines()
 
-        animals = set([x.split()[0] for x in contents if bool(self.regexp.search(x))])
+        animal_regex = re.compile(self.name_match)
+        zone_regex = re.compile(r'zone')
+        milkshake_loc_regex = re.compile(r'^(Left|Right)')
+        time_regex = re.compile(r'secs')
+
+        animals = set([x.split()[0] for x in contents if bool(animal_regex.search(x))])
+        datadict = {}
+        for animal in animals:
+            datadict[animal] = pd.DataFrame(columns=['Bout type', 'Bout start', 'Bout end', 'Milkshake Location'])
+
+        zone = None
+        milkshake_loc = None
+        animal = None
+        time = None
+
+        for line in contents:
+            # Check zone
+            if bool(zone_regex.search(line)):
+                zone = self.clean_and_strip_string(line)
+
+            # Check milkshake location
+            elif bool(milkshake_loc_regex.search(line)):
+                milkshake_loc = self.clean_and_strip_string(line)
+
+            # Check animal location
+            elif bool(animal_regex.search(line)):
+                animal = self.clean_and_strip_string(line)
 
     def run(self):
-        self.compile_name_match()
         self.set_datapath()
         self.set_savefolder()
         if self.dtype == 'ethovision':
